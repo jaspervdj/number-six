83ea625086a269f57ceb57d1306e5be2e3b2dde1
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-09-06 22:44:43 +0200

    Shorten last.fm url's

--- NumberSix/Handlers/LastFm.hs
+++ NumberSix/Handlers/LastFm.hs
@@ -10,13 +10,17 @@ import Text.HTML.TagSoup
 
 import NumberSix.Irc
 import NumberSix.Util.Http
+import NumberSix.Util.BitLy
 
 lastFm :: String -> Irc String
-lastFm query = httpScrape url $ \tags -> fromMaybe "Not found" $ do
-    artist <- nextTagText tags "artist"
-    name <- nextTagText tags "name"
-    url <- nextTagText tags "url"
-    return $ query ++ " last listened to: " ++ name ++ " by " ++ artist
+lastFm query = do
+    Just (text, longUrl) <- httpScrape url $ \tags -> do
+        artist <- nextTagText tags "artist"
+        name <- nextTagText tags "name"
+        url <- nextTagText tags "url"
+        return (query ++ " last listened to: " ++ name ++ " by " ++ artist, url)
+    shortUrl <- shorten longUrl
+    return $ text ++ " - " ++ shortUrl
   where
     url =  "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user="
         ++ urlEncode query ++ "&api_key=87b8b81da496639cb5a295d78e5f8f4d"
