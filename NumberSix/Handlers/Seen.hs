c8e403f8d2cf38b3cdae5d73a5ce5c23505cc8b8
Jasper Van der Jeugt <jaspervdj@gmail.com>
2011-01-20 14:28:21 +0100

    Port seen plugin to SQL

--- NumberSix/Handlers/Seen.hs
+++ NumberSix/Handlers/Seen.hs
@@ -13,27 +13,59 @@ import NumberSix.IrcString
 import NumberSix.Message
 import NumberSix.Bang
 import NumberSix.Util
-import NumberSix.Util.Redis
+import NumberSix.Util.Sql
 import NumberSix.Util.Time
 
 handler :: Handler ByteString
-handler = makeHandler "seen" [storeHook, loadHook]
+handler = makeHandlerWith "seen" [storeHook, loadHook] initialize
+
+initialize :: Irc ByteString ()
+initialize = withSqlRun
+    "CREATE TABLE seen (                    \
+    \    id SERIAL,                         \
+    \    host TEXT, channel TEXT,           \
+    \    sender TEXT, time TEXT, text TEXT  \
+    \)"
 
 storeHook :: Irc ByteString ()
 storeHook = onCommand "PRIVMSG" $ do
+    host <- getHost
+    channel <- getChannel
     sender <- toLower <$> getSender
-    time <- getTime
+    IrcTime time <- getTime
     text <- getMessageText
-    let lastSeen = (time, text)
-    withRedis $ \redis -> setItem redis ChannelRealm sender lastSeen
+    _ <- withSql $ \c -> do
+        r <- quickQuery' c
+            "SELECT id FROM seen  \
+            \WHERE host = ? AND channel = ? AND sender = ?"
+            [toSql host, toSql channel, toSql sender]
+        case r of
+            -- In the database: update
+            [[id']] -> run c
+                "UPDATE seen SET time = ?, text = ? WHERE id = ?"
+                [toSql time, toSql text, id']
+            -- Not yet in the database: insert
+            _ -> run c
+                "INSERT INTO seen (host, channel, sender, time, text)  \
+                \VALUES (?, ?, ?, ?, ?)"
+                [ toSql host, toSql channel, toSql sender
+                , toSql time, toSql text ]
+    return ()
 
 loadHook :: Irc ByteString ()
 loadHook = onBangCommand "!seen" $ do
-    (who, _) <- first toLower . breakWord <$> getBangCommandText
-    item <- withRedis $ \redis -> getItem redis ChannelRealm who
-    case item of
-        Just (time, text) -> do
-            pretty <- prettyTime time
-            writeReply $ "I last saw " <> who <> " " <> pretty
-                <> " saying: " <> text
-        _ -> writeReply $ "I ain't never seen " <> who
+    host <- getHost
+    channel <- getChannel
+    (sender, _) <- first toLower . breakWord <$> getBangCommandText
+    r <- withSql $ \c -> quickQuery' c
+        "SELECT time, text FROM seen  \
+        \WHERE host = ? AND channel = ? AND sender = ?"
+        [toSql host, toSql channel, toSql sender]
+    case r of
+        -- In the database: seen
+        [[time, text]] -> do
+            pretty <- prettyTime $ IrcTime $ fromSql time
+            writeReply $ "I last saw " <> sender <> " " <> pretty
+                                       <> " saying: " <> fromSql text
+        -- Not yet in the database: not seen
+        _ -> writeReply $ "I ain't never seen " <> sender
