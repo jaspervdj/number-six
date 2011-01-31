b39f8276dc35f095d23e83e233b9e9857fdbda5a
Andy Georges <andy.georges@elis.ugent.be>
2011-01-31 16:32:47 +0100

    flickr handler: code cleanup.

--- NumberSix/Handlers/Flickr.hs
+++ NumberSix/Handlers/Flickr.hs
@@ -7,6 +7,7 @@ module NumberSix.Handlers.Flickr
 import Control.Monad
 import qualified Control.Monad.Trans as CMT
 import Data.Maybe(fromJust)
+import System.Random (randomRIO)
 
 import Flickr.Monad
 import Flickr.People
@@ -25,12 +26,12 @@ type URL = String
 
 -- | get the public photos for a given user by the user's URL
 --
-publicPhotos :: URL -> FM  (Maybe [Photo])
+publicPhotos :: URL -> FM [Photo]
 publicPhotos userURL = do
     u <- tryFlick $ lookupUser userURL 
     case u of
-      Left err -> return Nothing
-      Right user -> liftM Just $ getPublicPhotos (userId user) Nothing []
+      Left err -> return []
+      Right user -> getPublicPhotos (userId user) Nothing []
 
 -- | Actual handler implementation.
 -- XXX: Needs cleanup and more functionality
@@ -38,22 +39,17 @@ flickr :: String -> Irc String String
 flickr query = do
     let user : _ = words query
         url = buildUserURL user
-    photos <- CMT.liftIO $ flickAPI hsflickrAPIKey $ publicPhotos url -- >>= \ps -> mapM (\p -> liftM photoDetailsURLs $ Flickr.Photos.getInfo (photoId p) Nothing) ps
-    case photos of
-      Nothing -> do report ("Did not find any photos for " ++ user) 
-                    textAndUrl ("Something went wrong when looking up the photos of " ++ user) url
-      Just [] -> textAndUrl ("No photos available for " ++ user) ""
-      Just ps -> do report ("Found " ++ (show $ length ps) ++ " photos for " ++ user)
-                    let p = head ps 
-                    photoUrl <- CMT.liftIO . flickAPI hsflickrAPIKey . liftM photoDetailsURLs $ Flickr.Photos.getInfo (photoId p) Nothing
-                    case photoUrl of
-                      [] -> textAndUrl ("Cannot obtain URL for the last photo of " ++ user) ""
-                      us -> let us' = filter (\u -> urlDetailsType u == "photopage") us
-                            in case us' of
-                              [] -> textAndUrl ("Cannot obtain URL for the last photo of " ++ user) ""
-                              (u:_) -> textAndUrl ("Last photo of " ++ user ++ ": ") (urlDetailsURL u)
-      _ -> do textAndUrl ("Flickr plugin: this should not happen") ""
-              
+    photoURLs <- CMT.liftIO $ flickAPI hsflickrAPIKey $ publicPhotos url >>= \ps -> mapM (\p -> liftM photoDetailsURLs $ Flickr.Photos.getInfo (photoId p) Nothing) ps
+    report ("Hey ... we're still living ... looking up for " ++ user)
+    case photoURLs of
+      (us:_) -> let us' = filter (\u -> urlDetailsType u == "photopage") us
+                in case us' of 
+                      (u:_) -> textAndUrl ("Last photo of " ++ user ++ ": ") (urlDetailsURL u)
+                      _ -> do report "OOPS"
+                              textAndUrl ("Cannot obtain URL for the last photo of " ++ user) ""
+      [] -> do report ("Did not find any photos for " ++ user)
+               textAndUrl ("Cannot obtain URL for the last photo of " ++ user) ""
+
   where buildUserURL u = "http://flickr.com/photos/" ++ u
 
 
