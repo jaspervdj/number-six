6572260f7c77e28bd5c43259e7577a8b12c25b6a
Jasper Van der Jeugt <jaspervdj@gmail.com>
2011-05-10 23:21:05 +0200

    There you go relix

--- NumberSix/Handlers/Twitter.hs
+++ NumberSix/Handlers/Twitter.hs
@@ -2,10 +2,11 @@ module NumberSix.Handlers.Twitter
     ( handler
     ) where
 
-import Data.Maybe (fromMaybe)
 import Data.Char (isDigit)
+import Data.List (isPrefixOf)
 
 import Text.HTML.TagSoup
+import Text.Regex.PCRE
 
 import NumberSix.Irc
 import NumberSix.Message
@@ -13,24 +14,28 @@ import NumberSix.Bang
 import NumberSix.Util
 import NumberSix.Util.Http
 
-getTweet :: Maybe String -> [Tag String] -> String
-getTweet muser tags =
-    let text = innerText $ insideTag "text" tags
-        user = case innerText (insideTag "screen_name" tags) of
-                    "" -> fromMaybe "" muser
-                    x  -> x
-    in if null text || null user then "Not found"
-                                 else "@" <> user <> ": " <> removeNewlines text
+getTweet :: [Tag String] -> String
+getTweet tags =
+    let text = removeNewlines $ innerText $ insideTag "text" tags
+        user = innerText $ insideTag "screen_name" tags
+    in if null text || null user
+        then "Not found"
+        else if "RT " `isPrefixOf` text
+            then text
+            else "@" <> user <> ": " <> text
 
 twitter :: String -> Irc String String
-twitter argument = if all isDigit argument
-    then httpScrape tweet $ getTweet Nothing
-    else httpScrape user $ getTweet $ Just argument
+twitter argument
+    | all isDigit argument = httpScrape (tweet argument) getTweet
+    | argument =~ ".*/status/[0-9]+$" = httpScrape (tweet fromUrl) getTweet
+    | otherwise = httpScrape (user argument) getTweet
   where
-    user  =  "http://api.twitter.com/1/statuses/user_timeline.xml?screen_name="
-          <> urlEncode argument <> "&include_rts=1&count=1"
-    tweet =  "http://api.twitter.com/1/statuses/show/"
-          <> urlEncode argument <> ".xml"
+    fromUrl = reverse $ takeWhile (/= '/') $ reverse argument
+
+    user u = "http://api.twitter.com/1/statuses/user_timeline.xml?screen_name="
+           <> urlEncode u <> "&include_rts=1&count=1"
+    tweet t =  "http://api.twitter.com/1/statuses/show/"
+            <> urlEncode t <> ".xml"
 
 handler :: Handler String
 handler = makeBangHandler "twitter" ["!twitter"] twitter
