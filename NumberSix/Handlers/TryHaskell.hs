bea703e46cb5843fd44992b66c8c326689b68a4d
Jasper Van der Jeugt <jaspervdj@gmail.com>
2011-06-05 11:21:15 +0200

    Decent JSON library for TryHaskell

--- NumberSix/Handlers/TryHaskell.hs
+++ NumberSix/Handlers/TryHaskell.hs
@@ -5,28 +5,35 @@ module NumberSix.Handlers.TryHaskell
     ( handler
     ) where
 
-import Control.Applicative ((<$>))
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (mzero)
 
 import Data.ByteString (ByteString)
-import Text.JSON
 import qualified Data.ByteString.Char8 as B
+
+import Data.Aeson (FromJSON, Value (..), parseJSON, (.:))
 
 import NumberSix.Irc
 import NumberSix.Message
 import NumberSix.Bang
+import NumberSix.Util
 import NumberSix.Util.Http
 
+-- | Result: type, value
+--
+data Result = Result ByteString ByteString
+
+instance FromJSON Result where
+    parseJSON (Object o) = Result <$> o .: "type" <*> o .: "result"
+    parseJSON _          = mzero
+
 eval :: ByteString -> Irc ByteString
-eval query = do
-    json <- decode . B.unpack <$> httpGet url
-    return $ either (const complain) id $ resultToEither $ do
-        JSObject object <- json
-        flip valFromObj object $ if ":t" `B.isPrefixOf` query then "type"
-                                                              else "result"
+eval query = httpGet url >>= \bs -> return $ case parseJsonEither bs of
+    Left _ -> "I'm a cybernetic lifeform node. Spare me your rubbish."
+    Right (Result t r) -> if ":t" `B.isPrefixOf` query then t else r
   where
-    url =  "http://tryhaskell.org/haskell.json?method=eval&expr="
-        <> urlEncode query
-    complain = "I'm a cybernetic lifeform node. Spare me your rubbish."
+    url = "http://tryhaskell.org/haskell.json?method=eval&expr=" <>
+        urlEncode query
 
 handler :: Handler
 handler = makeBangHandler "tryhaskell" [">", "!haskell"] eval
