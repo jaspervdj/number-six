66f74873e33d2d0b724a2acd01e6b1477d16a5bf
Jasper Van der Jeugt <jaspervdj@gmail.com>
2011-06-05 11:41:00 +0200

    Refactor Reddit handler to aeson

--- NumberSix/Handlers/Reddit.hs
+++ NumberSix/Handlers/Reddit.hs
@@ -3,11 +3,12 @@ module NumberSix.Handlers.Reddit
     ( handler
     ) where
 
-import Control.Monad (forM)
-import Text.JSON
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (mzero)
+import qualified Data.Map as M
+import Data.Aeson (FromJSON (..), Object, Value (..), (.:))
 
 import Data.ByteString (ByteString)
-import qualified Data.ByteString.Char8 as B
 
 import NumberSix.Irc
 import NumberSix.Bang
@@ -15,20 +16,30 @@ import NumberSix.Util
 import NumberSix.Util.BitLy
 import NumberSix.Util.Http
 
+data Reddit = Reddit [Link] deriving (Show)
+
+instance FromJSON Reddit where
+    parseJSON (Object o) = let o' = unData o in Reddit <$> o' .: "children"
+    parseJSON _          = mzero
+
+data Link = Link ByteString ByteString deriving (Show)
+
+instance FromJSON Link where
+    parseJSON (Object o) =
+        let o' = unData o in Link <$> o' .: "title" <*> o' .: "url" 
+    parseJSON _          = mzero
+
+-- | Fetch the data attribute from an object. Reddit's ugly json...
+unData :: Object -> Object
+unData o = case M.lookup "data" o of Just (Object o') -> o'; _ -> M.empty
+
 reddit :: ByteString -> Irc ByteString
-reddit query = do
-    Ok (JSObject root) <- fmap (decode . B.unpack) $ httpGet url
-    let Ok urls = do
-            JSObject data_ <- valFromObj "data" root
-            JSArray children <- valFromObj "children" data_
-            forM children $ \(JSObject child) -> do
-                JSObject childData <- valFromObj "data" child
-                url' <- valFromObj "url" childData
-                title <- valFromObj "title" childData
-                return (B.pack $ fromJSString title, B.pack $ fromJSString url')
-    randomElement urls >>= uncurry textAndUrl
+reddit query = httpGet url >>= \bs -> case parseJsonEither bs of
+        Left _ -> return "Reddit is down, keep refreshing!"
+        Right (Reddit l) -> randomElement l >>= textAndUrl'
   where
     url = "http://reddit.com/r/" <> query <> ".json"
+    textAndUrl' (Link t u) = textAndUrl t u
 
 handler :: Handler
 handler = makeBangHandler "reddit" ["!reddit"] reddit
