53d1e44cc95495d0bdab2d87c74e48107a5e5e87
Jasper Van der Jeugt <m@jaspervdj.be>
2012-05-20 10:25:34 +0200

    Fix some warnings

--- src/NumberSix/Handlers/Tumblr.hs
+++ src/NumberSix/Handlers/Tumblr.hs
@@ -1,31 +1,32 @@
 -- | Get a tumblr posts for a given user
---
 {-# LANGUAGE OverloadedStrings #-}
 module NumberSix.Handlers.Tumblr
     ( handler
     ) where
 
-import Control.Applicative ((<$>))
-import Control.Monad(forM)
-import Data.List (stripPrefix)
-import Data.Maybe (fromMaybe)
 
-import Data.ByteString (ByteString)
+--------------------------------------------------------------------------------
+import           Control.Applicative   ((<$>))
+import           Control.Monad(forM)
+import           Data.ByteString       (ByteString)
 import qualified Data.ByteString.Char8 as B
-import Text.JSON
+import           Text.JSON
 
-import NumberSix.Irc
-import NumberSix.Bang
-import NumberSix.Util
-import NumberSix.Util.BitLy
-import NumberSix.Util.Http
 
+--------------------------------------------------------------------------------
+import           NumberSix.Bang
+import           NumberSix.Irc
+import           NumberSix.Util
+import           NumberSix.Util.BitLy
+import           NumberSix.Util.Http
+
+
+--------------------------------------------------------------------------------
 -- | Tumblr gives a string containing the JSON value, but assign it to a
 -- variable in a JavaScript like approach, defining the variable
 -- tumblr_api_read to use further in other JS scripts on the caling page.
 -- Obviously, this approach is pretty detrimental for our purpose, so we need
 -- to clean the returned string before passing it on to the JSON decoder.
--- 
 cleanTumblrJSON :: ByteString -> ByteString
 cleanTumblrJSON str
     | tumblrPrefix `B.isPrefixOf` str = B.reverse $ B.tail $
@@ -34,9 +35,10 @@ cleanTumblrJSON str
   where
     tumblrPrefix = "var tumblr_api_read = "
 
+
+--------------------------------------------------------------------------------
 -- | Get a random tumble from the user, taken from the last 'count' tumbles. To
 -- obtain the last tumble, just pass 1 as the count.
---
 randomTumble :: ByteString -> Int -> Irc ByteString
 randomTumble query count = do
     result <- decode . B.unpack . cleanTumblrJSON <$> httpGet url
@@ -55,6 +57,8 @@ randomTumble query count = do
     url = "http://" <> query <> ".tumblr.com/api/read/json?num=" <>
         B.pack (show count)
 
+
+--------------------------------------------------------------------------------
 tumblr :: ByteString -> Irc ByteString
 tumblr query = 
     let command : user = B.words query
@@ -62,5 +66,7 @@ tumblr query =
         "last" -> randomTumble (head user) 1
         _ -> randomTumble query 50
 
+
+--------------------------------------------------------------------------------
 handler :: UninitiazedHandler
 handler = makeBangHandler "tumblr" ["!tumblr"] tumblr
