6de0e465bb65bfc2cc01230a26a6c5a5f58c01c5
Jasper Van der Jeugt <m@jaspervdj.be>
2012-05-23 19:59:16 +0200

    Port Wikipedia plugin to xmlhtml

--- src/NumberSix/Handlers/Wikipedia.hs
+++ src/NumberSix/Handlers/Wikipedia.hs
@@ -6,39 +6,38 @@ module NumberSix.Handlers.Wikipedia
 
 
 --------------------------------------------------------------------------------
+import           Control.Applicative ((<$>))
 import           Control.Monad.Trans (liftIO)
 import           Data.ByteString     (ByteString)
-import qualified Data.ByteString     as B
-import           Text.HTML.TagSoup
+import           Data.Maybe          (fromMaybe)
+import qualified Data.Text           as T
+import qualified Data.Text.Encoding  as T
+import           Text.XmlHtml
+import           Text.XmlHtml.Cursor
 
 
 --------------------------------------------------------------------------------
 import           NumberSix.Bang
 import           NumberSix.Irc
 import           NumberSix.Message
-import           NumberSix.Util
 import           NumberSix.Util.Http
 
 
 --------------------------------------------------------------------------------
 wiki :: ByteString -> IO ByteString
-wiki query = do
-    bodyContent <- httpScrape url $
-        dropWhile (~/= TagOpen ("div" :: ByteString) [("id", "bodyContent")])
-    let shortContent = innerText $ insideTag "p" bodyContent
-    return $ removeNewlines $ if " may refer to:" `B.isSuffixOf` shortContent
-        then innerText $ insideTag "li" bodyContent
-        else shortContent
+wiki query = fmap (T.encodeUtf8 . fromMaybe "Not found") $
+    httpGetScrape Html url $ \cursor -> do
+        body  <- findRec (byTagNameAttrs "div" [("id", "bodyContent")]) cursor
+        short <- nodeText . current <$> findRec (byTagName "p") body
+        if " may refer to:" `T.isSuffixOf` short
+            then nodeText . current <$> findRec (byTagName "li") body
+            else return short
   where
-    url =  "http://en.wikipedia.org/w/index.php?title=Special%3ASearch&search="
-        <> urlEncode query
+    url =
+        "http://en.wikipedia.org/w/index.php?title=Special%3ASearch&search=" <>
+        urlEncode query
 
 
 --------------------------------------------------------------------------------
 handler :: UninitializedHandler
-handler = makeBangHandler "wikipedia" ["!w", "!wik", "!wiki"] $ \query ->
-    liftIO $ do
-        result <- wiki query
-        return $ case result of
-            "" -> "Not found"
-            x  -> x
+handler = makeBangHandler "wikipedia" ["!w", "!wik", "!wiki"] $ liftIO . wiki
