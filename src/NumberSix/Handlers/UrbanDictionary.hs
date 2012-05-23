8a33985bdf860510556600e7da1455b0f36b7866
Jasper Van der Jeugt <m@jaspervdj.be>
2012-05-23 19:27:09 +0200

    Port UrbanDictionary handler to xmlhtml

--- src/NumberSix/Handlers/UrbanDictionary.hs
+++ src/NumberSix/Handlers/UrbanDictionary.hs
@@ -8,7 +8,10 @@ module NumberSix.Handlers.UrbanDictionary
 --------------------------------------------------------------------------------
 import           Control.Monad.Trans (liftIO)
 import           Data.ByteString     (ByteString)
-import           Text.HTML.TagSoup
+import           Data.Maybe          (fromMaybe)
+import qualified Data.Text.Encoding  as T
+import           Text.XmlHtml
+import           Text.XmlHtml.Cursor
 
 
 --------------------------------------------------------------------------------
@@ -20,16 +23,15 @@ import           NumberSix.Util.Http
 
 --------------------------------------------------------------------------------
 urban :: ByteString -> IO ByteString
-urban query = httpScrape url $ innerText . insideTag "div" .
-    dropWhile (~/= TagOpen ("div" :: ByteString) [("class", "definition")])
+urban query = do
+    result <- httpGetHtmlScrape url $ \cursor -> do
+        def <- findRec (byTagNameAttrs "div" [("class", "definition")]) cursor
+        return $ T.encodeUtf8 $ nodeText $ current def
+    return $ fromMaybe "Look it up yourself" result
   where
     url = "http://www.urbandictionary.com/define.php?term=" <> urlEncode query
 
 
 --------------------------------------------------------------------------------
 handler :: UninitializedHandler
-handler = makeBangHandler "urbandictionary" ["!urban"] $ \query -> liftIO $ do
-    result <- urban query
-    return $ case result of
-        "" -> "Not found"
-        x  -> x
+handler = makeBangHandler "urbandictionary" ["!urban"] $ liftIO . urban
