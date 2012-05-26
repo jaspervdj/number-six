ae3d805936a311ce762bdc50c8869385f8785541
Jasper Van der Jeugt <m@jaspervdj.be>
2012-05-26 17:34:58 +0200

    Add some errors

--- src/NumberSix/Util/Error.hs
+++ src/NumberSix/Util/Error.hs
@@ -24,8 +24,20 @@ errors =
     , "Socket fault, initializing postcard fallback"
     , "Error: datacenter under alien attack"
     , "Error: please insert more coins"
-    , "Error: please don't do that"
+    , "Error: please don't do that, it really hurts :-("
     , "Error: error occurred while printing error message"
+    , "OHHHH... I give up Core dumped"
+    , "COMPILER UNABLE TO ABORT RUN FAST AND DON'T LOOK BACK"
+    , "This is exactly the 1000th error, contact Javache for your price!"
+    , "Maybe you should try asking a human?"
+    , "Your request was bad, and you should feel bad"
+    , "can’t go mucking with a 'void *'"
+    , "I wondered how long it would take you to mess up this badly..."
+    , "Invalid command. Feel ashamed for yourself and try again."
+    , "Not tonight, I've got a headache"
+    , "This command is available. BUT NOT FOR YOU."
+    , "Error: too many regrets"
+    , "Oh non! Il y a eu une erreur!"
     ]
 
 
