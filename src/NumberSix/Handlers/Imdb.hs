00d623793e7181eb0326abe2145128609e323a86
Jasper Van der Jeugt <m@jaspervdj.be>
2013-01-17 11:47:06 +0100

    Use omdbapi for imdb plugin

--- src/NumberSix/Handlers/Imdb.hs
+++ src/NumberSix/Handlers/Imdb.hs
@@ -13,6 +13,7 @@ import           Control.Monad.Trans   (liftIO)
 import           Data.Aeson            (FromJSON (..), Value (..), (.:))
 import           Data.ByteString       (ByteString)
 import qualified Data.ByteString.Char8 as BC
+import           Data.Char             (isDigit)
 
 
 --------------------------------------------------------------------------------
@@ -28,20 +29,32 @@ import           NumberSix.Util.Http
 --------------------------------------------------------------------------------
 data Movie = Movie
     { movieTitle  :: ByteString
-    , movieYear   :: Int
-    , movieRating :: Double
-    , movieUrl    :: ByteString
+    , movieYear   :: ByteString
+    , movieRating :: ByteString
+    , movieVotes  :: ByteString
+    , movieId     :: ByteString
     } deriving (Show)
 
 
 --------------------------------------------------------------------------------
 instance FromJSON Movie where
     parseJSON (Object o) = Movie
-        <$> o .: "title"
-        <*> o .: "year"
-        <*> o .: "rating"
-        <*> o .: "imdb_url"
+        <$> o .: "Title"
+        <*> o .: "Year"
+        <*> o .: "imdbRating"
+        <*> o .: "imdbVotes"
+        <*> o .: "imdbID"
     parseJSON _          = mzero
+
+
+--------------------------------------------------------------------------------
+-- | Parse optional trailing year
+parseQuery :: ByteString -> (ByteString, Maybe ByteString)
+parseQuery bs = case reverse (BC.words bs) of
+    (year : title)
+        | BC.all isDigit year -> (BC.unwords (reverse title), Just year)
+        | otherwise           -> (bs, Nothing)
+    _                         -> (bs, Nothing)
 
 
 --------------------------------------------------------------------------------
@@ -49,17 +62,19 @@ imdb :: ByteString -> IO ByteString
 imdb query = do
     json <- http url id
     case parseJsonEither json of
-        Right (m : _) -> textAndUrl
-            (movieTitle m <>
-                " (" <> BC.pack (show $ movieYear m) <>
-                "): " <> BC.pack (show $ movieRating m))
-            (movieUrl m)
-        _             -> randomError
+        Right m -> textAndUrl
+            (movieTitle m <> " ("
+                <> movieYear m <> "): " <>
+                movieRating m <> " (" <>
+                movieVotes m <> " votes)")
+            ("http://www.imdb.com/title/" <> movieId m <> "/")
+        _       -> randomError
   where
-    url = "http://imdbapi.org/" <>
-        "?title=" <> urlEncode query <>
-        "&limit=1" <>
-        "&type=json"
+    (title, myear) = parseQuery query
+    url            =
+        "http://omdbapi.com/" <>
+            "?t=" <> urlEncode title <>
+            maybe "" ("&y=" <>) myear
 
 
 --------------------------------------------------------------------------------
