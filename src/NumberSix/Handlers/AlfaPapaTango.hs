e9f8e985ec452ecdfae2a8fed654f872df8e8d13
Andy Georges <itkovian@gmail.com>
2013-07-05 15:39:53 +0200

    Separate words in reply

--- src/NumberSix/Handlers/AlfaPapaTango.hs
+++ src/NumberSix/Handlers/AlfaPapaTango.hs
@@ -22,35 +22,35 @@ alfaPapaTangoHook = onBangCommand "!nato" $ do
 
 letterMap :: M.Map Char BC.ByteString
 letterMap = M.fromList $ zip ['a'..'z'] $ map BC.pack
-    [ "alfa"
-    , "bravo"
-    , "charlie"
-    , "delta"
-    , "echo"
-    , "foxtrot"
-    , "golf"
-    , "hotel"
-    , "india"
-    , "julliett"
-    , "kilo"
-    , "lima"
-    , "mike"
-    , "november"
-    , "oscar"
-    , "papa"
-    , "quebec"
-    , "romeo"
-    , "sierra"
-    , "tango"
-    , "uniform"
-    , "victor"
-    , "whiskey"
-    , "xray"
-    , "yankee"
-    , "zulu"
+    [ " alfa"
+    , " bravo"
+    , " charlie"
+    , " delta"
+    , " echo"
+    , " foxtrot"
+    , " golf"
+    , " hotel"
+    , " india"
+    , " julliett"
+    , " kilo"
+    , " lima"
+    , " mike"
+    , " november"
+    , " oscar"
+    , " papa"
+    , " quebec"
+    , " romeo"
+    , " sierra"
+    , " tango"
+    , " uniform"
+    , " victor"
+    , " whiskey"
+    , " xray"
+    , " yankee"
+    , " zulu"
     ]
 
 translate :: BC.ByteString -> BC.ByteString
-translate letters = BC.concatMap (\c -> case M.lookup c letterMap of 
-                                            Just s -> s
-                                            Nothing -> BC.pack [c]) letters
+translate letters = BC.tail $ BC.concatMap (\c -> case M.lookup c letterMap of 
+                                                    Just s -> s
+                                                    Nothing -> BC.pack [' ',c]) letters
